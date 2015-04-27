7cd32b9cf3fed5ab5a00b3c4727656373d576f95
Nikhil Garg <nikhilgarg2@gmail.com>
2015-04-27 08:38:26 +0530

    vector

--- banker_sim.cpp
+++ banker_sim.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 int main(){
-	int t=0, p, r, R, Time;
+	int t=0, p, r, Time;
+	int R[r];
 	cout<<"Enter number of process(p) to simulate\n";
 	cin>>p;
 	cout<<"Enter number(r) of resources\n";
 	cin>>r;
 	cout<<"Enter the resource(R) vector\n"
-	cin>>R;
+	for(int i=0;i<r;i++)
+		cin >> R[i];
 	cout<<"Enter the amount of time to simulate\n";
 	cin>>Time;
 	
