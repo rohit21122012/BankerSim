d683a0b6137500597a5483027a5d6e7cc32cdca1
Nikhil Garg <nikhilgarg2@gmail.com>
2015-04-27 08:54:52 +0530

    struct

--- banker_sim.cpp
+++ banker_sim.cpp
@@ -1,24 +1,46 @@
 #include <iostream>
 using namespace std;
 
+struct state
+{
+	int resource[100];
+	int claim[100][100];
+	int alloc[100][100];
+	int available[100];
+}intitial,temp;
+
+void saveState()
+{
+
+
+}
+
+bool isSafe()
+{
+
+	return true;
+}
+
 int main(){
+	
 	int t=0, p, r, Time;
-	int R[r];
+	
 	cout<<"Enter number of process(p) to simulate\n";
 	cin>>p;
 	cout<<"Enter number(r) of resources\n";
 	cin>>r;
-	cout<<"Enter the resource(R) vector\n"
+
+	cout<<"Enter the resource(R) vector\n";
 	for(int i=0;i<r;i++)
-		cin >> R[i];
+		cin >> intitial.resource[i];
 	cout<<"Enter the amount of time to simulate\n";
 	cin>>Time;
 	
 	while(t<Time){
 		saveState();
-		generateRequest();
+		//generateRequest();
 		if(!isSafe()){
-			restoreState();
+		//	restoreState();
 		}
 		t++;
 	}
